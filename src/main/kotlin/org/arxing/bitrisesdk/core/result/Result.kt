1ca5a2380918a0ec8302a07d8dcba954208169ee
Arxing <meiswind711108@gmail.com>
2021-08-13 21:10:51 +0800

    Implement custom Result<T>

--- src/main/kotlin/org/arxing/bitrisesdk/core/result/Result.kt
+++ src/main/kotlin/org/arxing/bitrisesdk/core/result/Result.kt
@@ -4,7 +4,7 @@ sealed class Result<T> {
 
   private class Success<T>(val value: T) : Result<T>()
 
-  private class Failure<T>(val error: Throwable) : Result<T>()
+  private class Failure<T>(val exception: Throwable) : Result<T>()
 
   companion object {
 
@@ -17,16 +17,59 @@ sealed class Result<T> {
     }
   }
 
-  fun toKotlinResult(): kotlin.Result<T> {
-    return when (this) {
-      is Success -> kotlin.Result.success(value)
-      is Failure -> kotlin.Result.failure(error)
+  val isSuccess: Boolean get() = this is Success
+
+  val isFailure: Boolean get() = this is Failure
+
+  fun getOrNull(): T? = when (this) {
+    is Failure -> null
+    is Success -> value
+  }
+
+  fun exceptionOrNull(): Throwable? = when (this) {
+    is Failure -> exception
+    is Success -> null
+  }
+
+  fun getOrThrow(): T = when (this) {
+    is Failure -> throw exception
+    is Success -> value
+  }
+
+  fun getOrElse(onFailure: (exception: Throwable) -> T): T = when (this) {
+    is Failure -> onFailure.invoke(exception)
+    is Success -> value
+  }
+
+  fun getOrDefault(defaultValue: T): T = when (this) {
+    is Failure -> defaultValue
+    is Success -> value
+  }
+
+  fun <R> map(transform: (T) -> R): Result<R> = when (this) {
+    is Failure -> failure(exception)
+    is Success -> success(transform(value))
+  }
+
+  fun onSuccess(action: (T) -> Unit): Result<T> = when (this) {
+    is Failure -> this
+    is Success -> {
+      action(value)
+      this
     }
+  }
+
+  fun onFailure(action: (Throwable) -> Unit): Result<T> = when (this) {
+    is Failure -> {
+      action(exception)
+      this
+    }
+    is Success -> this
   }
 
   override fun toString(): String {
     return when (this) {
-      is Failure -> "Failure($error)"
+      is Failure -> "Failure($exception)"
       is Success -> "Success($value)"
     }
   }
